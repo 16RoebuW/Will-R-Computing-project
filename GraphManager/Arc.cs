86329d1c51f400bdfc8191a7412201da2c51454a
16RoebuW <16RoebuW@fulford.york.sch.uk>
2022-10-24 18:41:41 +0100

    Added classes (attributes only)

---
+++ GraphManager/Arc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphManager
+{
+    class Arc
+    {
+        private string name;
+        private double weight;
+
+        public Node destination;
+    }
+}
---
+++ GraphManager/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphManager
+{
+    class Graph
+    {
+        private bool wasSaved;
+        private List<Node> nodes;
+        private int nodeID;
+
+        public double minWeight;
+    }
+}
